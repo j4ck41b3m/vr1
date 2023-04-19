ed5f6267f195b1af3482bec78054ef68a92f9598
j4ck41b3m <jorgeandr12@gmail.com>
2023-04-19 12:42:09 +0200

    casicasi

--- Assets/LOOKATME.cs
+++ Assets/LOOKATME.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class LOOKATME : MonoBehaviour
 {
+    public GameObject target;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.transform.SetParent(null);
     }
 
     // Update is called once per frame
@@ -15,5 +16,6 @@ public class LOOKATME : MonoBehaviour
     {
         GameObject player = GameObject.FindGameObjectWithTag("MainCamera");
         gameObject.transform.LookAt(player.transform);
+        gameObject.transform.position = new Vector3(target.transform.position.x, target.transform.position.y + 0.3f, target.transform.position.z);
     }
 }
--- Assets/Scripts/individual.cs
+++ Assets/Scripts/individual.cs
@@ -5,28 +5,41 @@ using UnityEngine;
 public class individual : MonoBehaviour
 {
     public GameObject controller;
-    public bool fell;
+    public bool hit, fell;
+    public AudioSource audi;
+    public AudioClip bonk;
     // Start is called before the first frame update
     void Start()
     {
+        hit = false;
         fell = false;
         controller = GameObject.Find("Controller");
+        audi = gameObject.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (transform.up.y < 0.9 && hit == false)
+        {
+            audi.PlayOneShot(bonk);
+            hit = true;
+        }
+
         if (transform.up.y < 0.3 && fell ==false)
         {
             Call();
             Destroy(gameObject, 0.3f);
         }
+
+       
+
     }
 
     void Call()
     {
+
         controller.GetComponent<bolos>().DetectarCaidos();
         fell = true;
-
     }
 }
